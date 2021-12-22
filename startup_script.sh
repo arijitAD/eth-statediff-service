8a82d4e510a1d5d8b87f46fa45d79bd56d9c0f3f
Arijit Das <arijitad.in@gmail.com>
2021-12-22 16:14:48 +0530

    Run image

--- startup_script.sh
+++ startup_script.sh
@@ -8,13 +8,5 @@ test "$VDB_COMMAND"
 set +e
 
 # docker must be run in privilaged mode for mounts to work
-echo "Setting up /app/geth-rw overlayed /app/geth-ro"
-mkdir -p /tmp/overlay && \
-sudo mount -t tmpfs tmpfs /tmp/overlay && \
-mkdir -p /tmp/overlay/upper && \
-mkdir -p /tmp/overlay/work && \
-mkdir -p /app/geth-rw && \
-sudo mount -t overlay -o lowerdir=/app/geth-ro,upperdir=/tmp/overlay/upper,workdir=/tmp/overlay/work /app/geth-rw && \
-
 echo "Running the statediff service" && \
 sudo ./eth-statediff-service "$VDB_COMMAND" --config=config.toml
