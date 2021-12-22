cc931de6a7b9acfa4f75b0197471151fb97d88d8
Arijit Das <arijitad.in@gmail.com>
2021-12-22 16:06:53 +0530

    Fix overlay.

--- startup_script.sh
+++ startup_script.sh
@@ -14,7 +14,7 @@ sudo mount -t tmpfs tmpfs /tmp/overlay && \
 mkdir -p /tmp/overlay/upper && \
 mkdir -p /tmp/overlay/work && \
 mkdir -p /app/geth-rw && \
-sudo mount -t overlay overlay -o lowerdir=/app/geth-ro,upperdir=/tmp/overlay/upper,workdir=/tmp/overlay/work /app/geth-rw && \
+sudo mount -t overlay -o lowerdir=/app/geth-ro,upperdir=/tmp/overlay/upper,workdir=/tmp/overlay/work /app/geth-rw && \
 
 echo "Running the statediff service" && \
 sudo ./eth-statediff-service "$VDB_COMMAND" --config=config.toml
